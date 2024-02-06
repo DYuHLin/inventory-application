32dc5efd42cf6a124c9f7c778c03d59b6624d5e5
DYuHLin <damianver2000@gmail.com>
2024-02-06 14:59:56 +0200

    Finished my last 2 models.

--- models/seller.js
+++ models/seller.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const SellerSchema = new Schema({
+    name: {type: String, required: true},
+    shoe: {type: Schema.Types.ObjectId, ref: 'Shoe', required: true},
+    established: {type: Date, default: Date.now}
+});
+
+SellerSchema.virtual("url").get(function () {
+    return `/catalog/seller/${this._id}`;
+});
+
+module.exports = mongoose.model("Seller", SellerSchema);
