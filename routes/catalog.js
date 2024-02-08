9cc22814c816e61c4def148ac7f897d3bc67a5f3
DYuHLin <damianver2000@gmail.com>
2024-02-08 15:57:54 +0200

    Finished setting up the data on the home page.

--- routes/catalog.js
+++ routes/catalog.js
@@ -10,10 +10,13 @@ router.get("/", shoe_controller.index);
 
 //shoe routes
 router.get("/shoes", shoe_controller.shoe_list_get);
+
 //brand routes
 router.get("/brands", brandController.brand_list_get);
+
 //seller routes
 router.get("/sellers", sellerController.sellers_list_get);
+
 //instance routes
 router.get("/instances", shoeinstanceController.shoeinstance_list_get);
 
